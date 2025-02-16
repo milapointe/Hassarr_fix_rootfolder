a44c3bf5469c93fc214f6d18bca7215cdd380c24
milapointe <77821029+milapointe@users.noreply.github.com>
2025-02-16 17:41:57 -0500

    Update services.py
    removed whitespace

--- custom_components/hassarr/services.py
+++ custom_components/hassarr/services.py
@@ -54,7 +54,6 @@ def handle_add_media(hass: HomeAssistant, call: ServiceCall, media_type: str, se
         _LOGGER.error("Title is missing in the service call data")
         return
    
-    
     _LOGGER.info(f"Title received: {title}")
 
     # Access stored configuration data
